c740b22ae0e51e7109b4d421c2a43f278976e69d
Haya Mamlouk <hayamamlouk25@gmail.com>
2023-10-17 23:05:42 +0200

    finish getPrior fct

--- projet2-3i005/projet.py
+++ projet2-3i005/projet.py
@@ -3,12 +3,32 @@
 
 import pandas as pd
 
-def getPrior(classe) :
-    data = pd.read_csv(classe)
-    nb_patient = len(data) - 1 
+def getPrior(data):
+    # NB : Les données sont déja chargé dans le ipnyb
+
+    # Calculer le nombre total de patients
+    nb_patient = len(data)
+
+    # Filtrer les lignes où la colonne "target" est égale à 1 (malades)
     target = data[data["target"] == 1]
-    nb_malades = target.count()
-    estimation = target / nb_patient
-    return estimation
+
+    # Calculer le nombre de malades
+    nb_malades = len(target)
+
+    # Calculer l'estimation des malades
+    estimation = nb_malades / nb_patient
+
+    # Calculer l'intervalle de confiance à 95% pour l'estimation
+    conf_int = (estimation - 1.96 * (estimation * (1 - estimation) / nb_patient) ** 0.5,
+                estimation + 1.96 * (estimation * (1 - estimation) / nb_patient) ** 0.5)
+
+    # Créer un dictionnaire contenant les résultats
+    result = {
+        'estimation': estimation,
+        'min5pourcent': conf_int[0],
+        'max5pourcent': conf_int[1]
+    }
+
+    return result
 
 
